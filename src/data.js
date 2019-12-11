4864d778a34f56a7edf127c2ed851ac6c2fdce4c
CamiPerezv <perezvargascf@gmail.com>
2019-12-11 15:12:33 -0300

    añadiendo estilos y readme

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 import POKEMON from './data/pokemon/pokemon.js'; // importando la funcion que filtra desde data
 
 export function filterPokemonType(type) { // exportando la funcion 
-  if( type == 'todos'){
+  if ( type == 'todos'){
       return POKEMON;
   } else {
     const resultadofiltrado = POKEMON.filter((resultadofiltrado) => resultadofiltrado.type == type);
