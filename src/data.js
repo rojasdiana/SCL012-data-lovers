5eb9f323d10d74fadc1aac6420c0e9827ee6257c
CamiPerezv <perezvargascf@gmail.com>
2019-12-20 14:10:52 -0300

    corrigiendo errores eslint

--- src/data.js
+++ src/data.js
@@ -1,10 +1,9 @@
 import POKEMON from './data/pokemon/pokemon.js'; // importando la funcion que filtra desde data
 
-export function filterPokemonType(type) { // exportando la funcion 
-  if ( type == 'todos'){
-      return POKEMON;
-  } else {
-    const resultadofiltrado = POKEMON.filter((resultadofiltrado) => resultadofiltrado.type == type);
-    return resultadofiltrado;
+export function filterPokemonType(type) { // exportando la funcion
+  if (type === 'todos') {
+    return POKEMON;
   }
+  const resultadoFilter = POKEMON.filter((resultadoPkn) => resultadoPkn.type === type);
+  return resultadoFilter;
 }
--- src/main.js
+++ src/main.js
@@ -1,40 +1,63 @@
 import { filterPokemonType } from './data.js';
-import pokemon from './data/pokemon/pokemon.js';
+
+function pokePoke(paramPoke) {
+  return `
+   <div class="wrap">
+     <div class="tarjeta">
+        <div class="infopoke">
+         <h4>${paramPoke.name}</h4>
+         <p>${paramPoke.num}</p>
+         <div class="imagenpoke">
+          <img class="grande" src="${paramPoke.img}" alt="imagenpoke"></img>
+          </div>
+          
+          
+        </div>
+         <div class="atras">
+           <img src="${paramPoke.img}" alt="imagen poke"></img>
+            <lo>
+             <li>Tipo: ${paramPoke.type}</li>
+             <li>Altura: ${paramPoke.height}</li>
+             <li>Peso: ${paramPoke.height}</li>
+             <li>Debilidad: ${paramPoke.weaknesses}</li>
+            </lo>
+         </div>
+     </div>
+   </div>`;
+}
 
 const planta = document.getElementById('planta');
-
 planta.addEventListener('click', () => {
-     
   const plantaValue = document.getElementById('planta').value;
   const resultado = filterPokemonType(plantaValue);
   const divFiltrados = document.getElementById('pokemonesFiltrados');
-  divFiltrados.innerHTML = ""; 
+  divFiltrados.innerHTML = '';
+
+
   for (let i = 0; i < resultado.length; i += 1) {
-    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`
+    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`;
   }
 });
 
 const agua = document.getElementById('agua');
-
 agua.addEventListener('click', () => {
   const aguaValue = document.getElementById('agua').value;
   const resultado = filterPokemonType(aguaValue);
   const divFiltrados = document.getElementById('pokemonesFiltrados');
-  divFiltrados.innerHTML = ""; 
+  divFiltrados.innerHTML = '';
   for (let i = 0; i < resultado.length; i += 1) {
-    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`
+    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`;
   }
 });
 
 const fuego = document.getElementById('fuego');
-
 fuego.addEventListener('click', () => {
   const fuegoValue = document.getElementById('fuego').value;
   const resultado = filterPokemonType(fuegoValue);
   const divFiltrados = document.getElementById('pokemonesFiltrados');
-  divFiltrados.innerHTML = ""; 
+  divFiltrados.innerHTML = '';
   for (let i = 0; i < resultado.length; i += 1) {
-    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`
+    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`;
   }
 });
 
@@ -43,9 +66,9 @@ aire.addEventListener('click', () => {
   const aireValue = document.getElementById('aire').value;
   const resultado = filterPokemonType(aireValue);
   const divFiltrados = document.getElementById('pokemonesFiltrados');
-  divFiltrados.innerHTML = ""; 
+  divFiltrados.innerHTML = '';
   for (let i = 0; i < resultado.length; i += 1) {
-    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`
+    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`;
   }
 });
 
@@ -54,9 +77,9 @@ electrico.addEventListener('click', () => {
   const electricoValue = document.getElementById('electrico').value;
   const resultado = filterPokemonType(electricoValue);
   const divFiltrados = document.getElementById('pokemonesFiltrados');
-  divFiltrados.innerHTML = ""; 
+  divFiltrados.innerHTML = '';
   for (let i = 0; i < resultado.length; i += 1) {
-    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`
+    divFiltrados.innerHTML += `${pokePoke(resultado[i])}`;
   }
 });
 
@@ -65,7 +88,7 @@ psiquico.addEventListener('click', () => {
   const psiquicoValue = document.getElementById('psiquico').value;
   const resultado = filterPokemonType(psiquicoValue);
   const divFiltrados = document.getElementById('pokemonesFiltrados');
-  divFiltrados.innerHTML = ""; 
+  divFiltrados.innerHTML = '';
   for (let i = 0; i < resultado.length; i += 1) {
     divFiltrados.innerHTML += `${pokePoke(resultado[i])}`;
   }
@@ -77,39 +100,9 @@ todos.addEventListener('click', () => {
   const todosValue = document.getElementById('todos').value;
   const resultado = filterPokemonType(todosValue);
   const divFiltrados = document.getElementById('pokemonesFiltrados');
-  divFiltrados.innerHTML = ""; 
+  divFiltrados.innerHTML = '';
 
   for (let i = 0; i < resultado.length; i += 1) {
     divFiltrados.innerHTML += `${pokePoke(resultado[i])}`;
-    
   }
 });
-
-
-function pokePoke(paramPoke){
- return`
-  <div class="wrap">
-    <div class="tarjeta">
-       <div class="infopoke">
-        <h4>${paramPoke.name}</h4>
-        <p>${paramPoke.num}</p>
-        <div class="imagenpoke">
-         <img class="grande" src="${paramPoke.img}" alt="imagenpoke"></img>
-         </div>
-         
-         
-       </div>
-        <div class="atras">
-          <img src="${paramPoke.img}" alt="imagen poke"></img>
-           <lo>
-            <li>Tipo: ${paramPoke.type}</li>
-            <li>Altura: ${paramPoke.height}</li>
-            <li>Peso: ${paramPoke.height}</li>
-            <li>Debilidad: ${paramPoke.weaknesses}</li>
-
-           </lo>
-        </div>
-    </div>
-  </div>`
-  
-}
